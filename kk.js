4a536ab5119a942b7d93fc652643e96778be5cf4
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2021-05-03 11:11:22 +0530

    commit

--- kk.js
+++ kk.js
@@ -1,4 +1,16 @@
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+
 exports.handler = async (event) => {
-    
-    return {"message": "Successfully executed"};
+    try {
+        let data = await s3.listObjects({
+            Bucket: "com.sigma.test.263248768798.asankha.us-east-1",
+            MaxKeys: 10
+        }).promise();
+
+    } catch (err) {
+        // error handling goes here
+    };
+
+    return { "message": "Successfully executed" };
 };
